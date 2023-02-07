cf6d899065906cf3af33fa7b872dcdd4b9e9f019
Ruslan Mukhamadiarov <Ruslan.Mukhamadiarov@th-nb-lfymbp52.local>
2023-02-07 08:44:22 +0100

    changes in python code

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,7 @@
 import torch as T
 import torch.nn as nn
 import time
+import matplotlib.pyplot as plt
 
 start_time = time.time() # time your simulation
 
@@ -9,17 +10,21 @@ input_size = 1
 output_size = 1
 num_epochs = 60
 learning_rate = 0.001
+a=15
+b=1
+r1=0
+r2=10
 
 # Sample dataset
-x_train = T.randn(15,1, requires_grad=True)
-y_train = T.randn(15,1)
+#x_train = -10*T.randn(15,1, requires_grad=True) + 10 # creates random distribution in range 0:10
+x_train = T.FloatTensor(a, b).uniform_(r1, r2) # creates random distribution in range 0:10
+y_train = T.FloatTensor(a, b).uniform_(r1, r2)
 
 # Linear regression model
 model = nn.Linear(input_size, output_size)
 
 # Optimizer
 optimizer = T.optim.SGD(model.parameters(), lr=learning_rate) # give network an optimizer
-
 
 print("Training...\n")
 
@@ -37,9 +42,22 @@ for epoch in range(0, num_epochs):
     optimizer.step()
 
     if ((epoch + 1) % 5 == 0):
-        print("Epoch [", (epoch + 1), "/", num_epochs, "], Loss: ", loss, "\n")
+        print("Epoch [", (epoch + 1), "/", num_epochs, "], Loss: ", loss.item())
     
-
-
 print("Training finished!\n")
 print("Python execution time: %s seconds " % (time.time() - start_time))
+
+output_filename = "output/linear_reggression.png"
+plt.figure(figsize=(10,8))
+axes = plt.gca()
+plt.xlabel("X", fontsize=24)
+plt.ylabel("Y", fontsize=24)
+plt.xlim([r1,r2])
+plt.ylim([r1,r2])
+plt.tick_params(axis='both', which='major', direction = 'in', length = 10 , 
+    width = 1, labelsize=24, bottom = True, top = True, left = True, right = True)
+plt.scatter(x_train.detach().numpy(),y_train.detach().numpy(), c='k')
+plt.plot(x_train.detach().numpy(),output.detach().numpy())
+plt.tight_layout()
+plt.savefig(output_filename, format="png", dpi=600)
+plt.show()
